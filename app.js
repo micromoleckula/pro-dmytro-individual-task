25d98d29ce41383d32632d78d6ead181fe5f13d7
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-01-16 15:40:03 +0000

    features css

--- app.js
+++ app.js
@@ -4,26 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const menu = document.getElementById('menu');
 
   burger.addEventListener('click', () => {
-    // Меню становится активным
     menu.classList.add('active');
-
-    // Скрывается иконка бургер-меню
     burger.classList.add('hidden');
-
-    // Отображается кнопка закрытия
     closeButton.classList.add('active');
-    // Если она тоже скрыта изначально, убираем у нее класс .hidden
     closeButton.classList.remove('hidden');
   });
 
   closeButton.addEventListener('click', () => {
-    // Меню перестаёт быть активным
     menu.classList.remove('active');
-
-    // Появляется обратно бургер-меню
     burger.classList.remove('hidden');
-
-    // Кнопка закрытия исчезает
     closeButton.classList.remove('active');
     closeButton.classList.add('hidden');
   });
@@ -31,8 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
-
-  // При скроле проверяется, сколько уже прокручено
   window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
       header.classList.add('scrolled');
@@ -40,7 +27,4 @@ document.addEventListener('DOMContentLoaded', () => {
       header.classList.remove('scrolled');
     }
   });
-
-  // Ниже можно добавить остальную логику для бургера/меню, если это требуется
-  // ...
 });
